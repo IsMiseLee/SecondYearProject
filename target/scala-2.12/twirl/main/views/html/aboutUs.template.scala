46238857fe642321c04f5a5a52d96ef66e7afc90
Enda Lee <enda.lee@it-tallaght.ie>
2018-04-04 18:09:52 +0100

    Lee more style

--- target/scala-2.12/twirl/main/views/html/aboutUs.template.scala
+++ target/scala-2.12/twirl/main/views/html/aboutUs.template.scala
@@ -33,8 +33,31 @@ object aboutUs extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.Ht
 Seq[Any](format.raw/*1.32*/("""
 
 """),_display_(/*3.2*/main("About Us",member)/*3.25*/ {_display_(Seq[Any](format.raw/*3.27*/("""
-  """),format.raw/*4.3*/("""<h1>Welcome to Aural Soul</h1>
+  
 
+  """),format.raw/*6.3*/("""<div class="container">
+    <div class="row">
+      <div class="col-sm-4">
+        <h1>About us </h1>
+      <p>We are the newest music marketplace. We strive to give the best music shopping experience around</p>
+      <p>We hope you enjoy your stay </p>
+<br><br>
+      <p>Contact us : 09478746527645</p>
+      <br><br>
+      <br><br>
+      <p>
+        <a href=""""),_display_(/*17.19*/routes/*17.25*/.ProductCtrl.listProducts()),format.raw/*17.52*/(""""> 
+          <button class="btn btn-primary">Click to view Albums</button>
+        </a>
+      </p>
+    </div>
+      <div class="col-sm-8">
+
+      <img src="/assets/images/aboutus.png" >
+
+    </div>
+    </div>
+  </div>
   
 """)))}))
       }
@@ -52,11 +75,11 @@ Seq[Any](format.raw/*1.32*/("""
 
               /*
                   -- GENERATED --
-                  DATE: Tue Apr 03 17:28:28 IST 2018
+                  DATE: Wed Apr 04 18:07:55 IST 2018
                   SOURCE: /home/wdd/webapps/SecondYearProject/app/views/aboutUs.scala.html
-                  HASH: 53543ba51750895db4414c40a093ee1a79090b7f
-                  MATRIX: 963->1|1088->31|1116->34|1147->57|1186->59|1215->62
-                  LINES: 28->1|33->1|35->3|35->3|35->3|36->4
+                  HASH: bd3f481c75b73a4aaeeea6d3bfd33f0a37a5ef95
+                  MATRIX: 963->1|1088->31|1116->34|1147->57|1186->59|1219->66|1608->428|1623->434|1671->461
+                  LINES: 28->1|33->1|35->3|35->3|35->3|38->6|49->17|49->17|49->17
                   -- GENERATED --
               */
           
